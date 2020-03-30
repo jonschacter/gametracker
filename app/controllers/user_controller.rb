d195853413d7c84e12e73ce2ecb956f13c1c2cc5
jonschacter <schacter.jon@gmail.com>
2020-03-30 14:54:11 -0700

    add signup page and functionality

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,27 @@
+class UserController < ApplicationController
+    get "/signup" do
+        # if Helpers.logged_in?(session)
+        #     redirect "/home"
+        # end
+        erb :"users/signup"
+    end
+
+    post "/signup" do
+        if params[:first_name] == "" || params[:last_name] == "" || params[:email] == "" || params[:password] == "" || User.find_by(email: params[:email])
+            redirect "/signup"
+        else
+            user = User.create(first_name: params[:first_name].capitalize, last_name: params[:last_name].capitalize, email: params[:email], password: params[:password])
+            session[:user_id] = user.id
+            redirect "/home"
+        end
+    end
+
+    get "/home" do
+        if Helpers.logged_in?(session)
+            @user = Helpers.current_user(session)
+            erb :"users/home"
+        else
+            redirect "/login"
+        end
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
     has_secure_password
+
+    def fullname
+        "#{self.first_name} #{self.last_name}"
+    end
 end
