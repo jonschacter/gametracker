4ff84d261d89de40ddbe58c47dba889910397763
jonschacter <schacter.jon@gmail.com>
2020-03-31 16:22:18 -0700

    sort games index by gametype

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -3,6 +3,24 @@ class GameController < ApplicationController
         if Helpers.logged_in?(session)
             @user = Helpers.current_user(session)
             @games = Game.where(user_id: @user.id)
+            @boardgames = []
+            @videogames = []
+            @cardgames = []
+            @dicegames = []
+            @othergames = []
+            @games.each do |game|
+                if game.gametype == "board"
+                    @boardgames << game
+                elsif game.gametype == "video"
+                    @videogames << game
+                elsif game.gametype == "card"
+                    @cardgames << game
+                elsif game.gametype == "dice"
+                    @dicegames << game
+                else
+                    @othergames << game
+                end
+            end
             erb :"games/index"
         else
             redirect '/login'
