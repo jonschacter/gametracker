3d92e75a95292526452989d6df21760a2d6017b9
jonschacter <schacter.jon@gmail.com>
2020-04-01 12:55:22 -0700

    add gamesession new post route and functionality

--- app/controllers/gamesession_controller.rb
+++ app/controllers/gamesession_controller.rb
@@ -24,6 +24,37 @@ class GameSessionController < ApplicationController
         end
     end
 
+    post '/gamesessions/new' do
+        if params[:game][:name] == "New"
+            @game = Game.new(name: params[:new_game][:name])
+            @game.gametype = params[:new_game][:type]
+            @game.user_id = Helpers.current_user(session).id
+            @game.save 
+        else
+            @game = Game.find_by(name: params[:game][:name], user_id: Helpers.current_user(session).id)
+        end
+
+        @players = []
+        params[:gamesession][:players].each do |player|
+            if player == "Me"
+                @players << Player.find_or_create_by(name: "Current User")
+            elsif player != ""
+                @players << Player.find_or_create_by(name: player)
+            end
+        end
+
+        @winner = @players[params[:gamesession][:winner].to_i - 1]
+        
+        @gamesession = GameSession.new
+        @gamesession.date = params[:gamesession][:date]
+        @gamesession.game = @game
+        @gamesession.players = @players
+        @gamesession.winner = @winner
+        @gamesession.save
+
+        redirect "/gamesessions/#{@gamesession.id}"
+    end
+
     get '/gamesessions/:id' do
         @gamesession = GameSession.find_by_id(params[:id])
         if Helpers.logged_in?(session) && @gamesession.user == Helpers.current_user(session)
